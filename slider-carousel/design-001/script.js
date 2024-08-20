6c250981273288dbe11419403815517b581ff627
Kevin <dennisk.blas@gmail.com>
2024-08-19 22:15:09 -0500

    se aplicó efecto de pausar o seguir con el movimiento los slides

--- slider-carousel/design-001/script.js
+++ slider-carousel/design-001/script.js
@@ -1,1 +1,16 @@
+//Obteniendo el Slider en Movimiento
+const slideTrack = document.querySelector('.slide-track');
+//Obteniedo los slides
+const slides = document.querySelectorAll('.slide');
+
+
+slides.forEach(slide=>{
+    slide.addEventListener('click',()=>{
+        console.log('Hola Mundo');
+        slideTrack.style.animationPlayState = 'paused';
+    })
+    slide.addEventListener('mouseleave',()=>{
+        slideTrack.style.animationPlayState = 'running';
+    })
+})
 
