ca776791464f80a72a403128afa4698e5f2176a9
Kevin <dennisk.blas@gmail.com>
2024-09-03 23:44:22 -0500

    cambio de theme DARK y LIGHT

--- switchColor/switch-01/main.ts
+++ switchColor/switch-01/main.ts
@@ -9,3 +9,31 @@ if(btnToggle){
         }
     })
 }
+
+//OPTION THEME
+const btnTheme = document.querySelectorAll('.theme-option') as NodeListOf<HTMLButtonElement>;
+if(btnTheme){
+    btnTheme.forEach(btn=>{
+        btn.addEventListener('click',(event)=>{
+            const target = event.target as HTMLButtonElement;
+            const dataTheme = target.dataset.theme;
+            //ROOT CSS
+            const root = document.documentElement;
+            if(dataTheme === 'theme-dark'){
+                root.style.setProperty('--primary-color', '#000')
+                root.style.setProperty('--primary-color-hover', '#333')
+                root.style.setProperty('--primary-color-active', '#444')
+                root.style.setProperty('--secondary-color', '#fff')
+                root.style.setProperty('--secondary-color-hover', '#ccc')
+                root.style.setProperty('--secondary-color-active', '#aaa')
+            } else if(dataTheme == 'theme-light'){
+                root.style.setProperty('--primary-color', '#fff')
+                root.style.setProperty('--primary-color-hover', '#ccc')
+                root.style.setProperty('--primary-color-active', '#aaa')
+                root.style.setProperty('--secondary-color', '#000')
+                root.style.setProperty('--secondary-color-hover', '#333')
+                root.style.setProperty('--secondary-color-active', '#444')
+            }
+        })
+    })
+}
